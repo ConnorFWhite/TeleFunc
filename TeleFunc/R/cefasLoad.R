98b4c8f6c995c180ab30d34e73b358b5e7a170c5
Connor White <connor.white@gmail.com>
2019-01-15 13:55:09 -0500

    Adding in various functions that I have been accumulated in different scripts and updated read.cefas

--- TeleFunc/R/cefasLoad.R
+++ TeleFunc/R/cefasLoad.R
@@ -1,9 +1,10 @@
-
-
-read.cefas<-function(file){
+readCefas<-function(file,time=FALSE){
+  
+  ####Working on multiple time formats.....
+  tformats<-c("%d-%m-%Y %H:%M:%S","%d-%m-%y %H:%M:%S","%d/%m/%y %H:%M:%S","%d/%m/%Y %H:%M:%S")
   
   dat<-read.table(file,col.names=paste("column", 1:10, sep="_"),fill=TRUE,sep=",")
-  dat<-dat[(((which(dat[,1]=="Time Stamp")[1])-6):nrow(dat)),]
+  dat<-dat[(((grep(pattern = "Time Stamp", x = as.character(dat[,1]))[1])-6):nrow(dat)),]
   dat<-dat[,1:4]
   
   colnames(dat)<-c("DateTime","Chanel1","Chanel2", "Chanel3")
@@ -14,35 +15,40 @@ read.cefas<-function(file){
   dataOut <- list()
   for(i in 1:nrow(blocks)){
     datBl <- dat[blocks[i,1]:blocks[i,2],]
-    t1<-as.POSIXct(as.character(datBl[1,1]),format="%d/%m/%Y %H:%M:%S")
-    if(!is.na(t1)){
-      datBl[,1] <- as.POSIXct(as.character(datBl[,1]),format="%d/%m/%Y %H:%M:%S")
-    }else{
-      datBl[,1] <- as.POSIXct(strsplit(as.character(datBl[1,1]),split="\\.")[[1]][1],format="%m/%d/%Y %I:%M:%S %p") + seq(0,length.out=nrow(datBl),by=0.04)
+    if(time==TRUE){
+      tout<-NULL
+      for(ts in 1:length(tformats)){
+        tout<-c(tout,as.POSIXct(as.character(datBl[1,1]),format=tformats[ts]))
+      }
+      if(any(!is.na(tout))){
+        tformat<-tformats[which(!is.na(tout))]
+        datBl[,1] <- as.POSIXct(as.character(datBl[,1]),format=tformat)
+      }else{
+        datBl[,1] <- as.POSIXct(strsplit(as.character(datBl[1,1]),split="\\.")[[1]][1],format="%Y-%m-%d %I:%M:%S %p") + seq(0,length.out=nrow(datBl),by=0.04)
+      }
     }
+    
     datBl[,2] <- as.numeric(as.character(datBl[,c(2)]))
     datBl[,3] <- as.numeric(as.character(datBl[,c(3)]))
     datBl[,4] <- as.numeric(as.character(datBl[,c(4)]))
     colnames(datBl) <- sapply((dat[(blocks[i,1]-1),]),FUN=as.character)
+    colnames(datBl)[1]<-'Time Stamp'
     datBl <- datBl[,which(!is.na(datBl[1,]))]
     dataOut[i][[1]] <- datBl
     
     block <- as.character((dat[(blocks[i,1]-7),1]))
     var <- colnames(datBl)[2]
-    rate <- strsplit(as.character((dat[(blocks[i,1]-4),1])),split=" = ")[[1]][2]
-    start <- strsplit(as.character((dat[(blocks[i,1]-6),1])),split=" = ")[[1]][2]
-    end <- strsplit(as.character((dat[(blocks[i,1]-5),1])),split=" = ")[[1]][2]
-    npoint <- strsplit(as.character((dat[(blocks[i,1]-2),1])),split=" = ")[[1]][2]
+    rate <- ifelse(time,round(as.numeric(datBl[2,1])-as.numeric(datBl[1,1]),digits=3),NA)
+    start <- as.character(datBl[1,1])
+    end <- datBl[nrow(datBl),1]
+    npoint <- nrow(datBl)
     
     Meta<-c(block,var,rate,start,end,npoint)
     blockInfo<-rbind(blockInfo,Meta)
   }
   names(dataOut)<-sapply(c(1:nrow(blocks)),FUN=function(x){paste("Block",x,sep="")})
   dataOut$BlockInfo<-as.data.frame(blockInfo)
+  rownames(dataOut$BlockInfo)<-1:nrow(dataOut$BlockInfo)
   colnames(dataOut$BlockInfo)<-c("Block","Variable","Rate","Start","End","nPoints")
   return(dataOut)
-}
-
-process.cefas<-function(dat){
-  
 }
---
+++ TeleFunc/R/circLines.R
@@ -0,0 +1,36 @@
+circLines<-function(daty,datx,lims=c(-180,180),...){
+  ys<-diff(daty)
+  daty<-as.numeric(daty)
+  datx<-as.numeric(datx)
+  
+  if(any(abs(ys)>lims)){
+    
+    loc<-which(abs(ys)>((lims[2]-lims[1])/2))
+    datSeg<-data.frame(x0=datx[1:(length(datx)-1)],
+                       x1=datx[2:length(datx)],
+                       y0=daty[1:(length(datx)-1)],
+                       y1=daty[2:length(datx)])
+    segments(x0=datSeg[-loc,1],
+             x1=datSeg[-loc,2],
+             y0=datSeg[-loc,3],
+             y1=datSeg[-loc,4],...)
+    
+    for(i in 1:length(loc)){
+      segments(x0=datSeg[loc[i],1],
+               x1=(datSeg[loc[i],2] + datSeg[loc[i],1])/2,
+               y0=datSeg[loc[i],3],
+               y1=lims[which.min(c(abs(lims[2]-datSeg[loc[i],3]), abs(lims[1]-datSeg[loc[i],3])))],...
+      )
+      segments(x0=(datSeg[loc[i],2] + datSeg[loc[i],1])/2,
+               x1=datSeg[loc[i],2],
+               y0=lims[which.min(c(abs(lims[2]-datSeg[loc[i],4]), abs(lims[1]-datSeg[loc[i],4])))],
+               y1=datSeg[loc[i],4],...
+      )
+    }
+  }else{
+    segments(x0=datx[1:(length(datx)-1)],
+             x1=datx[2:length(datx)],
+             y0=daty[1:(length(datx)-1)],
+             y1=daty[2:length(datx)],...)
+  }
+}
